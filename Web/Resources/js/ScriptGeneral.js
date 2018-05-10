f302ff62a643f25595e14904bd60de832c0267e7
DanteVR <dantevilla006@gmail.com>
2018-05-10 10:18:42 -0500

    ScriptGeneral
    Clone de Combos n

--- Web/Resources/js/ScriptGeneral.js
+++ Web/Resources/js/ScriptGeneral.js
@@ -6,9 +6,9 @@ var tbody = tabla[0].getElementsByTagName('tbody');
 var totalCol = tbody[0].childNodes[1].children.length;
 var arrayElem = [];
 
-for (var i = 1 ; i <= totalCol; i++) {
+for (var i = 1; i <= totalCol; i++) {
     var campo = document.getElementById("campo" + i);
-    arrayElem.push([campo.firstChild.tagName,campo.firstChild.id]);
+    arrayElem.push([campo.firstChild.tagName, campo.firstChild.id]);
 }
 
 $(document).ready(function () {
@@ -16,7 +16,6 @@ $(document).ready(function () {
         idFila++;
         cFila++;
         funcNuevaLinea(idFila, tabla[0].id);
-        document.getElementById("Hc" + tabla[0].id).value = cFila;
     });
 });
 
@@ -35,12 +34,11 @@ function eliminar(id) {
         cFila = cFila - 1;
     }
     ReordenarId(idTabla);
-    document.getElementById("Hc" + tabla[0].id).value = cFila;
 }
 
 function Reordenar(idTabla) {
     var num = 1;
-    $id = "#"+idTabla+" tbody tr";
+    $id = "#" + idTabla + " tbody tr";
     $($id).each(function () {
         $(this).find("td p").text(num);
         num++;
@@ -48,37 +46,38 @@ function Reordenar(idTabla) {
 }
 
 function ReordenarId(idTabla) {
-    var r=1;
+    var r = 1;
     var aux = 0;
     var child;
     var idText;
     var pasoPri = 1;
-    for (var i = 1 ; i <= cFila+1 ; i++) {
-        if(pasoPri == 1){
-            for (var j = 1; j <= ((totalCol-1)*2)+1; j+=2) {
-                child = tbody[0].childNodes[i].childNodes[j].childNodes[0].firstChild;   
+    var tbody = tabla[0].getElementsByTagName('tbody');
+    for (var i = 1; i <= cFila + 1; i++) {
+        if (pasoPri == 1) {
+            for (var j = 1; j <= ((totalCol - 1) * 2) + 1; j += 2) {
+                child = tbody[0].childNodes[i].childNodes[j].childNodes[0].firstChild;
                 idText = arrayElem[aux][1];
-                idText = idText.substring(0,idText.length-1);
+                idText = idText.substring(0, idText.length - 1);
                 console.log(idText + r);
-                child.id= idText + r;
-                child.name= idText + r;
-                aux++;        
+                child.id = idText + r;
+                child.name = idText + r;
+                aux++;
             }
-            if(i == 1){
-                i = i +1;
+            if (i == 1) {
+                i = i + 1;
             }
             pasoPri = 2;
-        }   
-        else{
+        }
+        else {
             aux = 0;
-            for (var j = 0; j <= totalCol-2; j++) {
-                child = tbody[0].childNodes[i].childNodes[j].childNodes[0].firstChild;   
+            for (var j = 0; j <= totalCol - 2; j++) {
+                child = tbody[0].childNodes[i].childNodes[j].childNodes[0].firstChild;
                 idText = arrayElem[aux][1];
-                idText = idText.substring(0,idText.length-1);
+                idText = idText.substring(0, idText.length - 1);
                 console.log(idText + r);
-                child.id= idText + r;
-                child.name= idText + r;
-                aux++;        
+                child.id = idText + r;
+                child.name = idText + r;
+                aux++;
             }
         }
         r++;
@@ -87,34 +86,40 @@ function ReordenarId(idTabla) {
 
 function funcNuevaLinea(cont, idTabla) {
     var nuevaFila = "";
-    $("#"+idTabla+">tbody")
+    var contSelect = 0;
+    var idDiv = "";
+    $("#" + idTabla + ">tbody")
         .append
         (
         $('<tr>').attr('id', idFila)
             .append
             (
-                function(){
-                    for (var i = 0; i < totalCol; i++) {
-                        var tagName = arrayElem[i][0];
-                        var idText = arrayElem[i][1];
-                        idText = idText.substring(0,idText.length-1);
-                        switch(tagName){   
-                            case 'P':       nuevaFila = nuevaFila + "<td><div><p>"+idFila+"</p></div></td>";
-                                            break;
-                            case 'BUTTON':  nuevaFila = nuevaFila + "<td><div class='d-flex justify-content-center input-group input-group-sm'><button class='btn btn-sm btn-danger mr-sm-2 btn-sm' id='"+idText+idFila+"' onclick='eliminar(this.id)'>Eliminar</button></div></td>";
-                                            break;
-                            case 'INPUT':   nuevaFila = nuevaFila + "<td><div class='input-group input-group-sm'><input type='text' runat='server' id='"+idText+idFila+"' name='"+idText+idFila+"' class='form-control'/></div></td>"
-                                            break; 
-                            case 'SELECT':  nuevaFila = nuevaFila + "<td><div id='div" + idFila + "' class='input-group input-group-sm'><script type='text/javascript'>$('#div" + idFila + "').prepend($('#select1').clone().insertAfter('#div" + idFila + "').attr('id','" + idText + idFila + "').attr('name','" + idText + idFila + "').attr('runat','server'))</script></div></td>"
-                                            break;
-                        }    
+            function () {
+                for (var i = 0; i < totalCol; i++) {
+                    var tagName = arrayElem[i][0];
+                    var idText = arrayElem[i][1];
+                    if (tagName == 'SELECT') {
+                        contSelect++;
+                        idDiv = "div" + contSelect + "_"
                     }
-                    return nuevaFila;
+                    idText = idText.substring(0, idText.length - 1);
+                    switch (tagName) {
+                        case 'P': nuevaFila = nuevaFila + "<td><div><p>" + idFila + "</p></div></td>";
+                            break;
+                        case 'BUTTON': nuevaFila = nuevaFila + "<td><div class='d-flex justify-content-center input-group input-group-sm'><button class='btn btn-sm btn-danger mr-sm-2 btn-sm' id='" + idText + idFila + "' onclick='eliminar(this.id)'>Eliminar</button></div></td>";
+                            break;
+                        case 'INPUT': nuevaFila = nuevaFila + "<td><div class='input-group input-group-sm'><input type='text' runat='server' id='" + idText + idFila + "' name='" + idText + idFila + "' class='form-control'/></div></td>"
+                            break;
+                        case 'SELECT': nuevaFila = nuevaFila + "<td><div id='" + idDiv + idFila + "' class='input-group input-group-sm'><script type='text/javascript'>$('#" + idDiv + idFila + "').prepend($('#" + idText + "1').clone().insertAfter('#" + idDiv + idFila + "').attr('id','" + idText + idFila + "').attr('name','" + idText + idFila + "').attr('runat','server'))</script></div></td>"
+                            break;
+                    }
                 }
+                return nuevaFila;
+            }
             )
         );
     Reordenar(idTabla);
+    ReordenarId(idTabla);
 }
-
 
 
