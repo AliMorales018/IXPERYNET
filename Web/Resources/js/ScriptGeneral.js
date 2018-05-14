597702cccbc97fa05714118e04c86fea0641b499
DanteVR <dantevilla006@gmail.com>
2018-05-14 13:03:36 -0500

    Script Modificado

--- Web/Resources/js/ScriptGeneral.js
+++ Web/Resources/js/ScriptGeneral.js
@@ -1,20 +1,30 @@
 
 var idFila = 1;
 var cFila = 1;
-var tabla = document.getElementsByTagName('table');
-var tbody = tabla[0].getElementsByTagName('tbody');
-var totalCol = tbody[0].childNodes[1].children.length;
 var arrayElem = [];
-
-for (var i = 1; i <= totalCol; i++) {
-    var campo = document.getElementById("campo" + i);
-    arrayElem.push([campo.firstChild.tagName, campo.firstChild.id]);
-}
+var arrayElemV = [];
+var array = [];
+var totalCol;
+var tabla;
+var tbody;
 
 $(document).ready(function () {
+    tabla = document.getElementsByTagName('table');
+    tbody = tabla[0].getElementsByTagName('tbody');
+    totalCol = tbody[0].childNodes[1].children.length;
+
+    for (var i = 1; i <= totalCol; i++) {
+        var campo = document.getElementById("campo" + i);
+        arrayElem.push([campo.firstChild.tagName, campo.firstChild.id]);
+        if (campo.firstChild.id.substring(0, 1) == "V") {
+            var idTextV = campo.firstChild.id;
+            idTextV = idTextV.substring(0, idTextV.length - 1);
+            arrayElemV.push(idTextV);
+        }
+    }
+
     $('#btnNuevo').on('click', function () {
         idFila++;
-        cFila++;
         funcNuevaLinea(idFila, tabla[0].id);
         document.getElementById("Hc" + tabla[0].id).value = cFila;
     });
@@ -61,7 +71,6 @@ function ReordenarId(idTabla) {
                 child = tbody[0].childNodes[i].childNodes[j].childNodes[0].firstChild;
                 idText = arrayElem[aux][1];
                 idText = idText.substring(0, idText.length - 1);
-                console.log(idText + r);
                 child.id = idText + r;
                 child.name = idText + r;
                 aux++;
@@ -77,7 +86,6 @@ function ReordenarId(idTabla) {
                 child = tbody[0].childNodes[i].childNodes[j].childNodes[0].firstChild;
                 idText = arrayElem[aux][1];
                 idText = idText.substring(0, idText.length - 1);
-                console.log(idText + r);
                 child.id = idText + r;
                 child.name = idText + r;
                 aux++;
@@ -91,6 +99,7 @@ function funcNuevaLinea(cont, idTabla) {
     var nuevaFila = "";
     var contSelect = 0;
     var idDiv = "";
+    cFila++;
     $("#" + idTabla + ">tbody")
         .append
         (
@@ -109,7 +118,7 @@ function funcNuevaLinea(cont, idTabla) {
                     switch (tagName) {
                         case 'P': nuevaFila = nuevaFila + "<td><div><p class='text-center'>" + idFila + "</p></div></td>";
                             break;
-                        case 'BUTTON': nuevaFila = nuevaFila + "<td><div class='d-flex justify-content-center input-group input-group-sm'><button class='btn btn-sm btn-danger mr-sm-2 btn-sm' id='" + idText + idFila + "' onclick='eliminar(this.id)'>Eliminar</button></div></td>";
+                        case 'BUTTON': nuevaFila = nuevaFila + "<td><div class='d-flex justify-content-center input-group input-group-sm'><button class='btn btn-sm btn-danger mr-sm-2 btn-sm' id='" + idText + idFila + "' onclick='eliminar(this.id)'><i class='icon icon-cross'></i></button></div></td>";
                             break;
                         case 'INPUT': nuevaFila = nuevaFila + "<td><div class='input-group input-group-sm'><input type='text' required='' runat='server' id='" + idText + idFila + "' name='" + idText + idFila + "' class='form-control'/></div></td>"
                             break;
@@ -126,7 +135,50 @@ function funcNuevaLinea(cont, idTabla) {
 }
 
 function ValidarCampos() {
-    document.getElementById(txtUsuario);
+    var obj;
+    var val;
+    var id;
+    document.getElementById("InputIsValid").value = "true";
+    array = ObtenerValores();
+    for (var a = 0; a < array.length; a++) {
+        for (var i = 1; i <= cFila; i++) {
+            obj = $("#" + arrayElemV[a] + i);
+            obj.removeClass("is-invalid");
+            val = obj.val().trim();
+            id = obj.attr('id');
+            for (var j = 1; j <= cFila; j++) {
+                if (array[a][j - 1][1] != id) {
+                    if (array[a][j - 1][0] == val) {
+                        document.getElementById("InputIsValid").value = "false";
+                        obj.addClass("is-invalid");
+                        obj.focus();
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    if (document.getElementById("InputIsValid").value = "true") {
+        return true;
+    }
 }
+
+function ObtenerValores() {
+    var arrayConjunto = [];
+    var Valor;
+    var Id;
+    for (var j = 0; j < arrayElemV.length; j++) {
+        var arrayValId = [];
+        for (var i = 1; i <= cFila; i++) {
+            obj = $("#" + arrayElemV[j] + i);
+            Valor = obj.val().trim();
+            Id = obj.attr('id');
+            arrayValId.push([Valor, Id]);
+        }
+        arrayConjunto.push(arrayValId);
+    }
+    return arrayConjunto;
+}
+
 
 
